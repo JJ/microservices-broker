fa868ee793ebe290a0d40cd6d27b62c32b868bbc
JJ Merelo <jjmerelo@gmail.com>
2018-10-25 19:33:57 +0200

    Procesando con rabbit

---
+++ github-server/procesa.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'bunny'
+require 'json'
+
+connection = Bunny.new(automatically_recover: false)
+connection.start
+
+channel = connection.create_channel
+queue = channel.queue('hello')
+
+
+begin
+  puts ' [*] Esperando mensajes. Interrumpe con ctrl-C'
+  queue.subscribe(block: true) do |_delivery_info, _properties, body|
+    puts " [x] Received #{body}"
+  end
+  
+rescue Interrupt => _
+  connection.close
+
+  exit(0)
+end
+
+
