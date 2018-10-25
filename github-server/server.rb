652525e0dc2841907a80c0c4c65ccb5595314d75
JJ Merelo <jjmerelo@gmail.com>
2018-10-25 18:47:02 +0200

    Depurando respuesta

--- github-server/server.rb
+++ github-server/server.rb
@@ -15,5 +15,6 @@ post '/' do
   piezas = push["compare"].split("/")
   api_url = "/repos/#{piezas[3]}/#{piezas[4]}/compare/#{piezas[6]}"
   diff = Net::HTTP.get(URI("https://api.github.com#{api_url}"))
-  pp(JSON.parse(diff.body))
+  puts diff
+  pp(JSON.parse(diff))
 end
