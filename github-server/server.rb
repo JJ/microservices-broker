b85f194b2b37891d7c9ea298703d6ae149e2d44b
JJ Merelo <jjmerelo@gmail.com>
2018-10-25 18:42:05 +0200

    Comentario

--- github-server/server.rb
+++ github-server/server.rb
@@ -11,12 +11,11 @@ require 'pp'
 
 set :port, 31415
 
-# Responde directamente desde el raíz
+# Descarga las diferencias hechas para un push
 post '/' do
   push = JSON.parse(request.body.read)
   piezas = push["compare"].split("/")
   api_url = "/repos/#{piezas[3]}/#{piezas[4]}/compare/#{piezas[6]}"
-
   diff = Net::HTTP.get(URI("https://api.github.com#{api_url}"))
   pp(JSON.parse(diff))
 end
