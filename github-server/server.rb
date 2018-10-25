69ecc2394ba2582b918c5271d577608d7c386985
JJ Merelo <jjmerelo@gmail.com>
2018-10-25 19:14:30 +0200

    Cambia a httparty

--- github-server/server.rb
+++ github-server/server.rb
@@ -4,7 +4,7 @@
 
 require 'sinatra'
 require 'json'
-require 'net/http'
+require 'httparty'
 require 'pp'
 
 set :port, 31415
@@ -14,7 +14,8 @@ post '/' do
   push = JSON.parse(request.body.read)
   piezas = push["compare"].split("/")
   api_url = "/repos/#{piezas[3]}/#{piezas[4]}/compare/#{piezas[6]}"
-  res = Net::HTTP.get_response(URI("https://api.github.com#{api_url}"))
-  pp( res.body )
-  diff_data = JSON.parse(eval(res.body))
+  res = HTTParty.get("https://api.github.com#{api_url}")
+  pp( res )
+  diff_data = response.parsed_response
+  pp(diff_data)
 end
