14b79bab985a97856c855ae52534baf112f624cc
JJ Merelo <jjmerelo@gmail.com>
2018-10-25 18:13:28 +0200

    Añade comentario

--- github-server/server.rb
+++ github-server/server.rb
@@ -8,10 +8,9 @@ require 'json'
 
 set :port, 31415
 
+# Responde directamente desde el raíz
 post '/' do
   push = JSON.parse(request.body.read)
   commit_sha = push.commit.sha
   puts "Commit → #{commit_sha}"
 end
-
-
