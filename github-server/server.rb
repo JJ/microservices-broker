4e658d5b866156f6741dca2cb7e310331e76d06b
JJ Merelo <jjmerelo@gmail.com>
2018-10-25 19:29:49 +0200

    Añade bunny

--- github-server/server.rb
+++ github-server/server.rb
@@ -6,6 +6,13 @@ require 'sinatra'
 require 'json'
 require 'httparty'
 require 'pp'
+require 'bunny'
+
+connection = Bunny.new(automatically_recover: false)
+connection.start
+
+channel = connection.create_channel
+queue = channel.queue('hello')
 
 set :port, 31415
 
@@ -13,7 +20,5 @@ set :port, 31415
 post '/' do
   push = JSON.parse(request.body.read)
   piezas = push["compare"].split("/")
-  api_url = "/repos/#{piezas[3]}/#{piezas[4]}/compare/#{piezas[6]}"
-  res = HTTParty.get("https://api.github.com#{api_url}")
-  pp( res["files"] )
+  channel.default_exchange.publish( "/repos/#{piezas[3]}/#{piezas[4]}/compare/#{piezas[6]}", routing_key: queue.name)
 end
