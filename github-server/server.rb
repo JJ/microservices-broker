3e591b7a23e48ed8976ca8824264b2aef38cc234
JJ Merelo <jjmerelo@gmail.com>
2018-10-25 18:14:22 +0200

    Arregla

--- github-server/server.rb
+++ github-server/server.rb
@@ -11,6 +11,6 @@ set :port, 31415
 # Responde directamente desde el raíz
 post '/' do
   push = JSON.parse(request.body.read)
-  commit_sha = push.commit.sha
+  commit_sha = push[:commit][:sha]
   puts "Commit → #{commit_sha}"
 end
