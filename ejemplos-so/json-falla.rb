db267d22077994eb1fb735447ef0c3101b2bc6f1
JJ Merelo <jjmerelo@gmail.com>
2018-10-25 20:14:27 +0200

    Ahora prueba segunda

--- ejemplos-so/json-falla.rb
+++ ejemplos-so/json-falla.rb
@@ -4,6 +4,8 @@ require 'json'
 require 'net/http'
 require 'pp'
 
+# Golf down to JSON parsing.
+
 diff = Net::HTTP.get(URI("https://api.github.com/repos/JJ/microservices-broker/compare/69ecc2394ba2...bf0cad05008c"))
 puts diff.class
 pp(JSON.parse(diff))
