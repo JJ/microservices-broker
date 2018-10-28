7e7e410ad4e24ad7251b2f553fb1357747bad1ba
JJ Merelo <jjmerelo@gmail.com>
2018-10-28 12:16:20 +0100

    Tratando de depurar esto

--- github-server/procesa.py
+++ github-server/procesa.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import pika
 
 # Create a global channel variable to hold our channel object in
@@ -8,18 +10,22 @@ def on_connected(connection):
     """Called when we are fully connected to RabbitMQ"""
     # Open a channel
     connection.channel(on_channel_open)
+    print( "Connected" )
 
-# Step #3
 def on_channel_open(new_channel):
     """Called when our channel has opened"""
     global channel
     channel = new_channel
     channel.queue_declare(queue="hook", durable=True, exclusive=False, auto_delete=False, callback=on_queue_declared)
+    print( "Channel open" )
+    
 
 # Step #4
 def on_queue_declared(frame):
     """Called when RabbitMQ has told us our Queue has been declared, frame is the response from RabbitMQ"""
-    channel.basic_consume(handle_delivery, queue='test')
+    channel.basic_consume(handle_delivery, queue='hook')
+    print( "Queue declared" )
+
 
 # Step #5
 def handle_delivery(channel, method, header, body):
