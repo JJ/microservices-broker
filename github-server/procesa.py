842ef91951e101aa7c8d44f01f92bae3d63e90b1
JJ Merelo <jjmerelo@gmail.com>
2018-10-28 12:48:51 +0100

    Añade URL

--- github-server/procesa.py
+++ github-server/procesa.py
@@ -14,7 +14,12 @@ print( "Channel open" )
 # Step #5
 def descarga(channel, method, properties, body):
     """Called when we receive a message from RabbitMQ"""
-    print(" [x] Recibido %r" % body.decode() )
+    url = body.decode()
+    print(" [x] Recibido %r" % url )
+    piezas = url.split("/")
+    api_url = "/repos/%r/%r/compare/%r" % piezas[3], piezas[4], piezas[5]
+    print(url)
+
 
 channel.basic_consume(descarga,
                       queue='hook',
